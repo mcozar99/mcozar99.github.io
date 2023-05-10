49795888173f54db38c22371bd35baa2b5d0170c
mcozar99 <mcozartramblin@hawk.iit.edu>
2023-05-10 18:09:05 -0500

    First Release

---
+++ README.txt
@@ -0,0 +1,18 @@
+My personal web. Where I expose all my projects, skills and experience. It is also the place where you can contact me for any inquiries.
+
+
+
+Credits:
+	Template:
+		Helios, by HTML5 Up
+
+	Images:
+		Unsplash
+
+	Icons:
+		Font Awesome (fontawesome.io)
+
+	Other:
+		jQuery (jquery.com)
+		Scrollex (github.com/ajlkn/jquery.scrollex)
+		Responsive Tools (github.com/ajlkn/responsive-tools)
---
+++ assets/js/read_more.js
@@ -0,0 +1,30 @@
+  const serviceItems = document.querySelector(".reel");
+  const popup = document.querySelector(".popup-box")
+  const popupCloseBtn = popup.querySelector(".popup-close-btn");
+  const popupCloseIcon = popup.querySelector(".popup-close-icon");
+
+  serviceItems.addEventListener("click",function(event){
+    if(event.target.tagName.toLowerCase() == "button"){
+       var item =event.target.parentElement;
+       var h3 = item.querySelector("h3").innerHTML;
+       var readMoreCont = item.querySelector(".read-more-cont").innerHTML;
+       popup.querySelector("h3").innerHTML = h3;
+       popup.querySelector(".popup-body").innerHTML = readMoreCont;
+       popupBox();
+    }
+
+  })
+
+  popupCloseBtn.addEventListener("click", popupBox);
+  popupCloseIcon.addEventListener("click", popupBox);
+
+  popup.addEventListener("click", function(event){
+     if(event.target == popup){
+        popupBox();
+     }
+  })
+
+  function popupBox(){
+    popup.classList.toggle("open");
+  }
+
